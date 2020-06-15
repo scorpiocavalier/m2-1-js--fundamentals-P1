ad4c13e8a9645b77ea99f55fe2877cf479c88e03
Arturo Chen <art.can.fly@gmail.com>
2020-06-15 19:49:09 -0400

    E4 done

--- workshop/exercise-4.js
+++ workshop/exercise-4.js
@@ -5,6 +5,6 @@
 // eg:
 // 0, 1, 4, 9, 16, ...
 
-for (let number = 0; number < 12; number++) {
-  console.log('the square of ', number, ' is ', square);
+for (let number = 0; number <= 12; number++) {
+  console.log(`The square of ${number} is ${number*number}`);
 }
