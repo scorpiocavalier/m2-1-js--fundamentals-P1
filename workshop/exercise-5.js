af6eaf462cd9e221d0ec8cfe0ff070e640b0312c
Arturo Chen <art.can.fly@gmail.com>
2020-06-15 19:53:15 -0400

    E5 done

--- workshop/exercise-5.js
+++ workshop/exercise-5.js
@@ -2,9 +2,7 @@
 // Fix this program
 // It should output all of the odd numbers between 1 and 25 (including 1 and 25)
 
-for (let number = 0; number < 25; number++) {
-  if (number % 2) {
+for (let number = 1; number <= 25; number++) {
+  if (number % 2 != 0)
     console.log(number);
-  }
-  console.log(number);
 }
