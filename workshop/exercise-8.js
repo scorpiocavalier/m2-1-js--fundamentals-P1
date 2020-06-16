22e48c947994e821f3179153d1148bc3b5e58522
Arturo Chen <art.can.fly@gmail.com>
2020-06-15 21:01:12 -0400

    E8 done

--- workshop/exercise-8.js
+++ workshop/exercise-8.js
@@ -3,3 +3,8 @@
 // between 0 and 5000
 
 // ANSWER: the number you should see in the console is 3127500
+let sum = 0;
+for(let num = 0; num <= 5000; num = num + 4)
+    sum += num;
+
+console.log(sum);
